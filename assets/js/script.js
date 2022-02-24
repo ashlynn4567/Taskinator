605b2e84c221df2994bf996355bb23d6994b172b
Ashley's PC <ashlynn4567@gmail.com>
2022-02-24 15:57:20 -0600

    add ability to add task with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+// find objects within the document and select them. save them as variables to use later
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+
+// function that will create a task in "Tasks To Do"
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+// on a button click ("Add Task"), callback createTaskHandler (create a task)
+buttonEl.addEventListener("click", createTaskHandler);
