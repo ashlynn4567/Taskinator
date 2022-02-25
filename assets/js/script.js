6283331d22ba5cdb19714c839eb8a255e26c0fd3
Ashley's PC <ashlynn4567@gmail.com>
2022-02-24 21:55:08 -0600

    added ability to track list items and types with button press

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,33 @@
 // find objects within the document and select them. save them as variables to use later
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 // function that will create a task in "Tasks To Do"
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name 
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + 
+    taskNameInput + "</h3><span class='task-type'>" + 
+    taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
 // on a button click ("Add Task"), callback createTaskHandler (create a task)
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
